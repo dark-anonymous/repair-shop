c428dcd33f95b0962b99db7236d5493dbd1a31b7
dark-anonymous <39450092+dark-anonymous@users.noreply.github.com>
2025-02-02 15:10:58 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(express.json());
 app.use(cors());
 
 const DATA_FILE = 'data.json';
+
+app.get('/', (req, res) => {
+    res.send('Backend berjalan dengan baik!');
+});
+
 // Get All Services
 app.get('/services', (req, res) => {
     const data = loadData(); // Load data dari fail JSON
